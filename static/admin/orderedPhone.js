f4c654c21a92eb70c0743f78ed39575bc4395ebb
Arham Amir <arhamamir424@gmail.com>
2023-03-05 21:20:42 +0500

    First Commit

---
+++ static/admin/orderedPhone.js
@@ -0,0 +1,85 @@
+let users = {}
+let allUsersId = []
+function getApi() {
+    fetch('http://127.0.0.1:8000/api')
+        .then(response => response.json())
+        .then((json) => {
+            users = json['Users']
+            allUsersId = Object.keys(users)
+
+            createRows()
+        })
+}
+
+
+
+function createRows() {
+    mainDiv = document.getElementById('mainDiv').innerHTML
+
+    allUsersId.forEach(uid => {
+        uOrderList = users[uid]['order']
+        if (uOrderList.length != 0 && uOrderList[0][0] != 'Empty') {
+            mainDiv += ` <div class="fluid-container">
+           <center>
+               <nav class="dopp navbar navbar-expand-lg navbar-dark
+                       justify-content-between text-white">
+                   <a class="navbar-brand" href="#">
+                       ${users[uid]['name']}
+                   </a>
+                   <button class="navbar-toggler " type="button" data-toggle="collapse"
+                       data-target= "${uid}+1" aria-controls="navbarNavDropdown01" aria-expanded="false"
+                       aria-label="Toggle navigation" style="outline-color:#fff">
+                       <span class="navbar-toggler-icon"></span>
+                   </button>
+
+                   <div class="collapse navbar-collapse" id="${uid}+1">
+
+                       <ul class="navbar-nav ">
+
+                           <!--dropdown item of menu-->
+                           <li class="nav-item dropdown">
+                               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   Mobiles
+                               </a>
+
+                               <!--dropdown sub items of menu-->
+                               <div id = "${uid}" class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                   <div class="dropdown-divider"></div>
+                               </div>
+                           </li>
+
+                       </ul>
+
+                   </div>
+                   <div> <a class = "btn btn-warning" onclick = "funn3('${uid}')" href="#">Dispatch Order</a></div>
+               </nav>
+           </center>
+       </div>
+            <hr>`;
+            document.getElementById('mainDiv').innerHTML = mainDiv
+            httt = document.getElementById(uid).innerHTML;
+            let ht = ``
+            uOrderList.forEach(or => {
+                console.log(or);
+                ht += `<a class="dropdown-item" href="#">
+                ${or[0]} - ${or[1]}     
+                </a>
+                <hr>`
+            })
+            document.getElementById(uid).innerHTML = (ht +httt)
+
+        }
+
+
+    });
+
+}
+
+
+getApi()
+
+
+function funn3(uid) {
+    window.location.href = `/admin/dispatchOrder/${uid}`
+}
